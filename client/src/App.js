1ed9a5a6461f69a17dea71425f9c4691326cbb98
Nelson <nmirandan90@gmail.com>
2019-04-21 18:38:47 -0600

    componentes login y register sin enlace con backend aun

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import register from "./components/auth/register";
+import login from "./components/auth/login";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Mi app en react</h1>
+        <Router>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={register} />
+            <Route exact path="/login" component={login} />
+          </div>
+          <Footer />
+        </Router>
       </div>
     );
   }
